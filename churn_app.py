59790b5c3bd26d960c80e54f9f520364291d29eb
Binoy-07 <108354873+Binoy-07@users.noreply.github.com>
2025-11-26 03:37:03 +0600

    Update supervisor title in churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -122,5 +122,5 @@ if st.button('♻️Predict Churn'):
 
 import streamlit as st
 st.title("Devloped by BINOY😉!")
-st.title("Supervised by: Avishek Chowdhury🎓")
+st.title("Supervised by: Avishek Chowdhury Sir🎓")
 # ...
